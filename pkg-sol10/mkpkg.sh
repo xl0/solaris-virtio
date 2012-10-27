6fbd81a3e31b786b668e34f4e0df67a4b8abcda0
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2012-10-27 03:29:33 +0300

    Clean up before generating the package

--- pkg-sol10/mkpkg.sh
+++ pkg-sol10/mkpkg.sh
@@ -4,6 +4,9 @@ cp ../virtio .
 cp ../blkdev .
 cp ../vioblk .
 
+rm -f SUNWvioblk vioblk-$VERSION-itu.iso 
+rm -fr /var/spool/pkg/SUNWviobl
+
 pkgmk -o
 pkgtrans /var/spool/pkg `pwd`/SUNWvioblk-$VERSION SUNWvioblk
 itu makedu -r 5.10 -o `pwd`/vioblk-$VERSION-itu.iso /var/spool/pkg/SUNWvioblk
