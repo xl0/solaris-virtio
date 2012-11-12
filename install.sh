6db9dbf4665eaf7d4d474a4a8d86b7ae1203973d
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2012-11-12 14:49:36 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-sudo cp vioblk blkdev /kernel/drv/amd64/
+# This script is intended for quick testing, use the
+# generated package to install the driver.
+
+
+sudo cp vioblk /kernel/drv/amd64/
 sudo cp virtio /kernel/misc/amd64/
+
+if [ -e blkdev ]
+then
+	sudo cp blkdev /kernel/drv/amd64/
+fi
+
+if [ -e blkdev_illumos ]
+then
+	sudo cp blkdev_illumos /kernel/drv/amd64/
+fi
